966aaaaf4b8c63fb7d031f7f15e0f699077d498c
TausifAli2000 <tausifali2000@gmail.com>
2024-11-08 17:19:33 +0530

    changes in note content

--- React Module Project/src/Components/NotesGroupContent/notesGroupContent.jsx
+++ React Module Project/src/Components/NotesGroupContent/notesGroupContent.jsx
@@ -1,33 +1,21 @@
-import React from 'react';
-import './notesGroupContent.css';
-import {assets} from '../../assets/assests.js'
-import GroupName from '../Sidebar/groupName.jsx';
+import React from "react";
+import "./DesktopNotesContent.css";
 
-const NotesGroupContent = () => {
+function DesktopNotesContent({ note }) {
+  
   return (
-    <>
-    <div className="notesContent-container">
-    <div className="content-header">
-        <GroupName />
-    </div>
-    
-    <div className="content-container">
-      <div className="content-notes"> <p>Another productive way to use this tool to begin a daily writing routine. 
-        One way is to generate a random paragraph with the intention to try to rewrite it while still keeping the original meaning. The purpose here is to just get the writing started so that when the writer goes 
-        onto their day's writing projects, words are already flowing from their fingers.</p>
+    <div className="desktop__notes__content__note">
+      <div className="desktop__notes__content__details">
+        <p>{note.content}</p>
       </div>
+      <div className="desktop__notes__content__date__time__details">
+        <div className="desktop__notes__content__date">{note.date}</div>
+        <div className="vector"> </div>
+        <div className="desktop__notes__content__time">{note.time}</div>
+      </div>
+      
     </div>
-    <div className="input-container">
-      <textarea placeholder='Enter your text here.....'>
-      </textarea>
-      <button className="createNote">
-        <img src={assets.create_disabled} />
-      </button>
-    </div>
-    </div>
-    
-    </>
-  )
+  );
 }
 
-export default NotesGroupContent
+export default DesktopNotesContent;
